aef4b6ef1e7040c8ab0e1108dc73a723fc233200
Ajaykrishnan R <110657810+TheAjaykrishnanR@users.noreply.github.com>
2025-04-09 04:10:17 +0530

    Create wezterm.lua
    first commit

---
+++ wezterm.lua
@@ -0,0 +1,75 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-nologo" }
+config.font = wezterm.font("JetBrains Mono", { weight = "DemiBold" })
+config.cell_width = 0.9
+config.tab_bar_at_bottom = true
+config.window_decorations = "RESIZE"
+config.colors = {
+	foreground = "#ebdbb2",
+	background = "#282828",
+	cursor_bg = "#ebdbb2",
+	cursor_border = "#ebdbb2",
+	cursor_fg = "#282828",
+	selection_bg = "#3c3836",
+	selection_fg = "#ebdbb2",
+
+	ansi = {
+		"#282828", -- black
+		"#cc241d", -- red
+		"#98971a", -- green
+		"#d79921", -- yellow
+		"#458588", -- blue
+		"#b16286", -- magenta
+		"#689d6a", -- cyan
+		"#a89984", -- white
+	},
+
+	brights = {
+		"#928374", -- bright black
+		"#fb4934", -- bright red
+		"#b8bb26", -- bright green
+		"#fabd2f", -- bright yellow
+		"#83a598", -- bright blue
+		"#d3869b", -- bright magenta
+		"#8ec07c", -- bright cyan
+		"#ebdbb2", -- bright white
+	},
+}
+
+-- my keybindings
+config.keys = {
+	{
+		key = "n",
+		mods = "CTRL|ALT",
+		action = wezterm.action.SplitHorizontal,
+	},
+	{
+		key = "h",
+		mods = "CTRL|ALT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|ALT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|ALT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|ALT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "b",
+		mods = "CTRL|ALT",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+}
+
+return config
